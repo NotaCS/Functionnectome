9794e65ec4aaea0f2cceee4313897e3ca9387fcd
Victor Nozais <victor.nozais@gmail.com>
2022-12-02 15:56:26 +0100

    Renaming of the disconnectome presence as DiscROver, and old Presence scores back in the WhiteRest tool (as optionals)(for real this time)

--- Functionnectome/whiteRest.py
+++ Functionnectome/whiteRest.py
@@ -17,8 +17,8 @@ using the same white matter priors as the one used to generate the WhiteRest atl
 Atlernatively, it is possible to replace the input ROI with the corresponding disconnectome (if it was already
 computed). In this case, the "--disco" option will need to be specified.
 
-WhiteRest will output a table with the Presence score for each RSN in the ROI,
-both in % and raw score, as well as a few other metrics.
+WhiteRest will output a table with the DiscROver score (and/or the Presence score) for each RSN in the ROI,
+both in % and raw (i.e. non-normalised) score, as well as a few other metrics.
 If no output file is given (with the "-o" option), the table will be printed in
 the terminal. Otherwise, it will be saved as a text file, which can be imported
 to a spreadsheet software (such as Excel) for further processing.
@@ -66,8 +66,14 @@ def _build_arg_parser():
                    help='Path to the atlas labels identifying the RSNs.')
 
     # Optional arguments
+    p.add_argument('-s', '--score',
+                   default="discrover",
+                   help=(
+                       'The score(s) to be computed. Can be "discrover", '
+                       '"presence", or "both" (default is "discrover")'
+                   ))
     p.add_argument('-ot', '--out_table',
-                   help='Path to save the presence results (.txt or .csv).')
+                   help='Path to save the score results (.txt or .csv).')
     p.add_argument('-od', '--out_disco',
                    help="Path to save the lesion's disconnectome (.nii or .nii.gz), if computed.")
     p.add_argument('-z', '--Z_thresh',
@@ -80,15 +86,29 @@ def _build_arg_parser():
                    help='Path to save a pie-chart figure of the results (.png).')
     p.add_argument('-pt', '--thr_low_pie',
                    default=5,
-                   help='Presence %% under which the RSNs are grouped on the pie-chart (default <5%%).')
+                   help='DiscROver %% under which the RSNs are grouped on the pie-chart (default <5%%).')
     p.add_argument('-d', '--disco',
                    action='store_true',
                    help='To be specified when the "in_ROI" input given is not the lesion but '
-                        'the disconncetome of the lesion')
+                        'the disconncetome of the lesion. Incompatible with Presence score computation.')
     p.add_argument('-m', '--multiproc',
                    default=1,
                    help='Number of processes to run in parallel (default = 1).')
     return p
+
+
+def checkScore(parser, score, disco, pie):
+    if score.lower() not in ('discrover', 'presence', 'both'):
+        parser.error(
+            f"The 'score' option must be 'discrover', 'presence', or 'both', but was '{score.lower()}'."
+        )
+    if score.lower() in ('presence', 'both') and disco:
+        parser.error(
+            "The computation of Presence score requires the lesion ROI as input, "
+            "but the 'disco' option was selected, indicating that the input is a disconnectome."
+        )
+    if score.lower() == 'presence' and pie:
+        parser.error("Sorry, the pie-chart option is not available with the Prensence score.")
 
 
 def checkOutFile(parser, path):
@@ -114,9 +134,9 @@ def checkInFile(parser, path):
         )
 
 
-def computPresence(ROI_f, atlas_f, RSNlabels_f, zThresh=7, binarize=False, saveDisco=None, disco=False, proc=1):
+def computDiscROver(ROI_f, atlas_f, RSNlabels_f, zThresh=7, binarize=False, saveDisco=None, disco=False, proc=1):
     '''
-    Compute the presence score
+    Compute the DiscROver score
 
     Parameters
     ----------
@@ -139,7 +159,7 @@ def computPresence(ROI_f, atlas_f, RSNlabels_f, zThresh=7, binarize=False, saveD
 
     Returns
     -------
-    A Pandas Dataframe with the presence score of each involved RSN
+    A Pandas Dataframe with the DiscROver score of each involved RSN
 
     '''
     atlas_im = nib.load(atlas_f)
@@ -185,65 +205,146 @@ def computPresence(ROI_f, atlas_f, RSNlabels_f, zThresh=7, binarize=False, saveD
         RSNlabels = RSNlabels.drop(RSNlabels.index[-1])
         atlas = atlas[:, :, :, :-1]
     atlas = np.nan_to_num(atlas)
-    totalPresRSN = [atlas[..., i].sum() for i in range(atlas.shape[-1])]
-    resPresence = pd.DataFrame(columns=('RSN number', 'RSN name', 'Presence (%)', 'Presence (raw)'))
+    resDiscROver = pd.DataFrame(columns=('RSN number', 'RSN name', 'DiscROver (%)', 'DiscROver (raw)'))
 
     atlas[atlas < zThresh] = 0
     if binarize:
         atlas[atlas > 0] = 1
 
-    # Presence computation, all RSNs at the same time
+    # DiscROver computation, all RSNs at the same time
     atlasDisco = atlas * np.expand_dims(discoV, -1)
-    totalPresRSN = atlas.sum((0, 1, 2))
-    rawPres = atlasDisco.sum((0, 1, 2))  # sum along the 3 spatial dimensions
-    presence = 100 * rawPres / totalPresRSN
+    totalDscvrRSN = atlas.sum((0, 1, 2))
+    rawDscvr = atlasDisco.sum((0, 1, 2))  # sum along the 3 spatial dimensions
+    discROver = 100 * rawDscvr / totalDscvrRSN
 
     for i in range(atlas.shape[-1]):
+        resDiscROver.loc[i] = [RSNlabels.loc[i, 'RSN number'],  # RSN number
+                               RSNlabels.loc[i, 'RSN name'],  # RSN name
+                               discROver[i],  # DiscROver (%)
+                               rawDscvr[i],  # DiscROver (raw)
+                               ]
+    resDiscROver.sort_values('DiscROver (%)', ascending=False, inplace=True, ignore_index=True)
+    return resDiscROver
+
+
+def computPresence(ROI_f, atlas_f, RSNlabels_f, zThresh=7, binarize=False):
+    '''
+    Compute the presence score
+    Parameters
+    ----------
+    ROI_f : str
+        Filepath of the input ROI file (3D nifti in MNI 2mm space).
+    atlas_f : str
+        Filepath of the RSN white matter atlas (4D nifti in MNI 2mm space).
+    RSNlabels_f : str
+        Filepath of the text file with the labels and names of each RSN.
+    options : dict
+        Dictionnary containing the values of all the options.
+            zThresh (float)
+            rsnThresh (if zThresh=0) in %
+            bin (if zThresh>0)
+            propRSN (if zThresh>0)
+            figs
+    Returns
+    -------
+    A Pandas Dataframe with the presence score of each involved RSN
+    '''
+    ROI_im = nib.load(ROI_f)
+    ROI = ROI_im.get_fdata().astype(bool)
+    ROI_affine = ROI_im.affine.copy()
+    atlas_im = nib.load(atlas_f)
+    atlas = atlas_im.get_fdata()
+    RSNlabels = pd.read_csv(RSNlabels_f, sep='\t')
+
+    if not (ROI_im.affine == atlas_im.affine).all():
+        ROI_affine[0] = -ROI_affine[0]
+        if (ROI_affine == atlas_im.affine).all():
+            ROI = np.flip(ROI)
+        else:
+            print("Orientation (affine) of the input ROI volume not recognized. "
+                  "Expected orientation matrix:")
+            print(atlas_im.affine)
+            print('Affine of the input ROI:')
+            print(ROI_im.affine)
+            raise ValueError('Wrong data orientation, or not in MNI152 space.')
+
+    if len(RSNlabels) != atlas.shape[3]:
+        raise IndexError('Number of RSNs in the atlas and the label file not the same')
+
+    if RSNlabels.iloc[-1]['RSN name'] == 'Cerebellum':  # Removing the cerebellum RSN from the analysis
+        RSNlabels = RSNlabels.drop(RSNlabels.index[-1])
+        atlas = atlas[:, :, :, :-1]
+    atlas = np.nan_to_num(atlas)
+    totalPresRSN = [atlas[..., i].sum() for i in range(atlas.shape[-1])]
+    resPresence = pd.DataFrame(columns=('RSN number', 'RSN name',
+                                        'Presence/RSN (%)', 'Presence prop. (%)', 'Presence (raw)',
+                                        'Coverage (%)'))
+
+    ROIinAtlas = atlas[ROI]  # 2D array ROIxRSN
+    ROIinAtlas[ROIinAtlas < zThresh] = 0
+    if binarize:  # Sould only be used if zThresh > 0, but I don't enforce it
+        ROIinAtlas[ROIinAtlas > 0] = 1
+        binMaps = ROIinAtlas
+    else:
+        binMaps = ROIinAtlas.copy()
+        binMaps[binMaps > 0] = 1
+
+    presenceRaw = ROIinAtlas.sum(0)
+    presenceRSNnorm = 100 * presenceRaw / totalPresRSN
+    presenceProp = 100 * np.divide(presenceRSNnorm,
+                                   presenceRSNnorm.sum(),
+                                   out=np.zeros_like(presenceRSNnorm),
+                                   where=presenceRSNnorm.sum() != 0)
+    coverage = 100 * binMaps.sum(0) / ROI.sum()
+
+    indRSNpresent = np.argwhere(presenceRaw).T[0]
+    for i in indRSNpresent:
         resPresence.loc[i] = [RSNlabels.loc[i, 'RSN number'],  # RSN number
                               RSNlabels.loc[i, 'RSN name'],  # RSN name
-                              presence[i],  # Presence (%)
-                              rawPres[i],  # Presence (raw)
-                              ]
+                              presenceRSNnorm[i],  # Presence (RSN)
+                              presenceProp[i],  # Presence (%)
+                              presenceRaw[i],  # Presence (raw)
+                              coverage[i]]  # Coverage
     resPresence.sort_values('Presence (%)', ascending=False, inplace=True, ignore_index=True)
     return resPresence
 
 
 def make_fun_autopct(res):
     def fun_autopct(pct):
-        res_array = res['Presence (%)'].values
+        res_array = res['DiscROver (%)'].values
         res_pct = 100 * res_array / res_array.sum()
         ind_pct = (np.abs(res_pct - pct)).argmin()  # Get the index closest to the input % (pct)
-        pres = res_array[ind_pct]
-        return "{:.1f}%\n({:.1f}%)".format(pres, pct)
+        dscvr = res_array[ind_pct]
+        return "{:.1f}%\n({:.1f}%)".format(dscvr, pct)
     return fun_autopct
 
 
 def make_fun_autopct_withThr(res):
     def fun_autopct(pct):
-        res_array = res['Presence (%)'].values
+        res_array = res['DiscROver (%)'].values
         res_pct = 100 * res_array / res_array.sum()
         ind_pct = (np.abs(res_pct - pct)).argmin()
         if ind_pct == 0:
             return "({:.1f})%".format(pct)
         else:
-            pres = res_array[ind_pct]
-            return "{:.1f}%\n({:.1f}%)".format(pres, pct)
+            dscvr = res_array[ind_pct]
+            return "{:.1f}%\n({:.1f}%)".format(dscvr, pct)
     return fun_autopct
 
 
 def plot_pie(res, outFile, thresh_percent):
-    lowrsn = (res['Presence (%)'] < thresh_percent)
+    lowrsn = (res['DiscROver (%)'] < thresh_percent)
     lowrsnNb = lowrsn.sum()
     res_thr = res.copy()
     sumLow = 0
-    if lowrsnNb > 1:  # If there are RSNs with presence under the threshold, group them together
+    if lowrsnNb > 1:  # If there are RSNs with DiscROver under the threshold, group them together
         for i in lowrsn[lowrsn].index:
-            sumLow += res.loc[i, 'Presence (%)']
+            sumLow += res.loc[i, 'DiscROver (%)']
             res_thr = res_thr.drop(i)
         res_thr = pd.concat(
             [
                 pd.DataFrame({'RSN number': [f'< {thresh_percent}% ({lowrsnNb} RSN)'],
-                              'Presence (%)': [sumLow]}
+                              'DiscROver (%)': [sumLow]}
                              ),
                 res_thr
             ], ignore_index=True)
@@ -252,7 +353,7 @@ def plot_pie(res, outFile, thresh_percent):
         expl = [0 for i in range(len(res_thr))]  # To put the low value appart in the pie-chart
         expl[0] = 0.1
         plt.figure(figsize=(10, 8), dpi=120)
-        patches, texts, autotexts = plt.pie(res_thr['Presence (%)'],
+        patches, texts, autotexts = plt.pie(res_thr['DiscROver (%)'],
                                             labels=res_thr['RSN number'],
                                             textprops={'fontsize': 12, 'font': 'Arial'},
                                             autopct=make_fun_autopct_withThr(res_thr),  # '%1.1f%%',
@@ -267,7 +368,7 @@ def plot_pie(res, outFile, thresh_percent):
         cmap = plt.get_cmap('Spectral')
         colors = [cmap(i) for i in np.linspace(0, 1, len(res))]
         plt.figure(figsize=(10, 8), dpi=120)
-        patches, texts, autotexts = plt.pie(res['Presence (%)'],
+        patches, texts, autotexts = plt.pie(res['DiscROver (%)'],
                                             labels=res['RSN number'],
                                             textprops={'fontsize': 12, 'font': 'Arial'},
                                             autopct=make_fun_autopct(res),  # '%1.1f%%',
@@ -283,6 +384,7 @@ def main():
     parser = _build_arg_parser()
     args = parser.parse_args()
 
+    score = args.score
     ROI_f = args.in_ROI
     atlas_f = args.atlas_maps
     RSNlabels_f = args.atlas_labels
@@ -292,6 +394,7 @@ def main():
     discoIn = args.disco
     proc = int(args.multiproc)
 
+    checkScore(parser, score, discoIn, args.out_pie)
     checkInFile(parser, ROI_f)
     checkInFile(parser, atlas_f)
     checkInFile(parser, RSNlabels_f)
@@ -301,16 +404,25 @@ def main():
     if args.out_pie:
         checkOutFile(parser, args.out_pie)
 
-    res = computPresence(ROI_f, atlas_f, RSNlabels_f, zThresh, binarize, svDisco, discoIn, proc)
+    if score.lower() in ('presence', 'both'):
+        resPres = computPresence(ROI_f, atlas_f, RSNlabels_f, zThresh, binarize)
+
+    if score.lower() in ('discrover', 'both'):
+        res = computDiscROver(ROI_f, atlas_f, RSNlabels_f, zThresh, binarize, svDisco, discoIn, proc)
+        if args.out_pie:
+            res = res.sort_values('DiscROver (%)').reset_index()
+            plot_pie(res, args.out_pie, float(args.thr_low_pie))
+        if score.lower() == 'both':
+            res = pd.concat(
+                [res.set_index('RSN number'), resPres.set_index('RSN number')],
+                axis=1).reset_index().fillna(0)  # 'reset_index' returns  'RSN number' to a column
+    else:  # score == 'presence'
+        res = resPres
 
     if args.out_table:
         res.to_csv(args.out_table, sep='\t')
     else:
         print(res)
-
-    if args.out_pie:
-        res = res.sort_values('Presence (%)').reset_index()
-        plot_pie(res, args.out_pie, float(args.thr_low_pie))
 
 
 # %%
